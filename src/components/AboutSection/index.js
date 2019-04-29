12ba0ef999b9b8ee78031d5ad3a11c52c4c63fd1
Luke Bennett <luke.bennett88@gmail.com>
2019-04-29 21:49:53 +1000

    Style AboutSection

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 
 const AboutSection = () => (
-  <section>
+  <section className="mb-24">
     <div className="max-w-lg mx-auto text-xl">
       <h2 className="font-display mb-2 text-6xl text-center">About Us</h2>
       <p>
         Port Macquarie Restaurant Bar Chop 'n Chill was created out of a passion
-        to deliver a relaxed dining experience in Port Macquarie unlike any
+        for delivering a relaxed dining experience in Port Macquarie unlike any
         other. Showcasing a classic feel-good fare that's executed with a
         playful twist. The atmosphere at Chop 'n Chill is infused with a relaxed
         beach style buzz, boasting spacious indoor and outdoor dining areas,
         floor to ceiling windows, and fabulous waterfront views. Let us fill
-        your soul with the perfect ambiance, food, and place to unwind. Pop in
-        for breakfast, lunch, dinner, or any time inbetween. If you're a local,
+        your soul with the perfect ambience, food, and place to unwind. Pop in
+        for breakfast, lunch, dinner, or any time in-between. If you're a local,
         we want you to feel like you're on holiday. If you're on holiday, we'd
         like you to enjoy this corner of our beautiful seaside town.
       </p>
       <p>
         For us, it's all about the party. Whatever type of party you imagine,
-        that's the type of party we'll create. Wheather it's a wedding or
+        that's the type of party we'll create. Whether it's a wedding or
         fundraiser, baby shower or 70th birthday party, we'll attend to every
         detail, so that your dream event becomes a reality worth remembering. We
-        take care of the details, so you can enjoy the event!
+        take care of the details so that you can enjoy the event!
       </p>
     </div>
   </section>
