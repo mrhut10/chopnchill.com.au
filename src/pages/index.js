6d13346163598cd06882c5ba4747cab6fce627f7
Luke Bennett <luke.bennett88@gmail.com>
2019-04-29 21:53:24 +1000

    Layout homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Footer from '../components/Footer';
+import Footer from '../components/Footer';
 
 import '../css/fonts.css';
 import '../css/tailwind.css';
@@ -11,17 +11,25 @@ import Hero from '../components/Hero';
 import Header from '../components/Header';
 import MenuSection from '../components/MenuSection';
 import AboutSection from '../components/AboutSection';
+import EventsSection from '../components/EventsSection';
+import ContactSection from '../components/ContactSection';
+import MapSection from '../components/MapSection';
+import Banner from '../components/Banner';
 
 const IndexPage = () => (
-  <div className="body antialiased flex flex-col font-sans leading-relaxed min-h-screen overflow-hidden relative text-brand-blue w-full">
+  <div className="antialiased flex flex-col font-sans leading-relaxed text-brand-blue w-full">
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <Hero />
     <Header />
     <main className="bg-white flex-1 mx-auto w-full">
       <MenuSection />
       <AboutSection />
+      <Banner>Indoors. Outdoors. Doggo Friendly. Call to Book a Table.</Banner>
+      <EventsSection />
+      <ContactSection />
+      <MapSection />
     </main>
-    {/* <Footer /> */}
+    <Footer />
   </div>
 );
 
