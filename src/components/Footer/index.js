778242d426bbdd4adcad32e71082a8051809b85c
Luke Bennett <luke.bennett88@gmail.com>
2019-04-29 21:51:50 +1000

    Update Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
-
-import Social from './Social';
+import { FaFacebookSquare, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => (
   <StaticQuery
@@ -15,24 +14,43 @@ const Footer = () => (
       }
     `}
     render={data => (
-      <footer className=" mx-auto w-full">
-        <Social />
-        <p className="flex flex-wrap items-center justify-center mb-0 p-4 text-center w-full">
-          <span className="flex-no-shrink w-full md:w-auto">
-            © {new Date().getFullYear()} {data.site.siteMetadata.title}
-          </span>
-          <span className="hidden md:block mx-2 w-full md:w-auto">|</span>
-          <span className="flex-no-shrink w-full md:w-auto">
-            Website by
-            <a
-              className="font-semibold inline-flex px-1"
-              href="https://phirannodesigns.com.au"
-              rel="nofollow"
-            >
-              Phiranno Designs
-            </a>
-          </span>
-        </p>
+      <footer>
+        <div className="bg-brand-blue flex flex-wrap justify-around leading-tight px-6 py-10 text-white">
+          <div className="px-3">
+            <h3 className="font-display mb-4 text-2xl">
+              We're <br />
+              Very Social
+            </h3>
+            <p>Follow us to keep updated</p>
+            <p className="flex items-center">
+              <FaInstagram className="mr-1" />
+              #chopnchill
+              <FaFacebookSquare className="ml-2 mr-1" />
+              @chopnchill
+            </p>
+          </div>
+          <div className="px-3">
+            <h3 className="font-display text-2xl">Contact</h3>
+          </div>
+          <div className="px-3">
+            <h3 className="font-display text-2xl">Hours</h3>
+          </div>
+          <div className="px-3">
+            <h3 className="font-display text-2xl">Navigation</h3>
+          </div>
+        </div>
+        <div className="flex flex-wrap items-center justify-center mb-0 p-4 text-center w-full">
+          © {new Date().getFullYear()} {data.site.siteMetadata.title}{' '}
+          <span className="mx-2">|</span>
+          Website by
+          <a
+            className="font-semibold inline-flex px-1"
+            href="https://phirannodesigns.com.au"
+            rel="nofollow"
+          >
+            Phiranno Designs
+          </a>
+        </div>
       </footer>
     )}
   />
