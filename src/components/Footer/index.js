7d04fa558472fe3dd7939937c9ce427c90b5656c
Luke Bennett <luke.bennett88@gmail.com>
2019-05-02 09:25:21 +1000

    Add more markup to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { graphql, StaticQuery } from 'gatsby';
+import { graphql, Link, StaticQuery } from 'gatsby';
 import { FaFacebookSquare, FaInstagram } from 'react-icons/fa';
+
+import Rosemary from '../../images/rosemary-footer.svg';
 
 const Footer = () => (
   <StaticQuery
@@ -8,39 +10,103 @@ const Footer = () => (
       query FooterQuery {
         site {
           siteMetadata {
-            title
+            email
+            facebook
+            instagram
             phone
             phoneFormatted
-            email
+            title
           }
         }
       }
     `}
     render={data => (
       <footer>
-        <div className="bg-brand-blue flex flex-wrap justify-around leading-tight px-6 py-10 text-white">
-          <div className="px-3">
-            <h3 className="font-display mb-4 text-2xl">
+        <div className="bg-brand-blue flex flex-wrap leading-tight px-6 py-10 relative text-white">
+          <div className="flex flex-col items-center mb-6 px-3 w-full sm:w-1/4">
+            <h3 className="font-display mb-4 text-2xl tracking-wider uppercase">
               We're <br />
               Very Social
             </h3>
             <p>Follow us to keep updated</p>
-            <p className="flex items-center">
-              <FaInstagram className="mr-1" />
-              #chopnchill
-              <FaFacebookSquare className="ml-2 mr-1" />
-              @chopnchill
+            <p className="flex">
+              <a
+                className="flex items-center mr-2"
+                href={data.site.siteMetadata.instagram}
+              >
+                <FaInstagram className="mr-1" />
+                #chopnchill
+              </a>
+              <a
+                className="flex items-center"
+                href={data.site.siteMetadata.facebook}
+              >
+                <FaFacebookSquare className="mr-1" />
+                @chopnchill
+              </a>
             </p>
           </div>
-          <div className="px-3">
-            <h3 className="font-display text-2xl">Contact</h3>
+          <div className="flex flex-col items-center mb-6 px-3 w-full sm:w-1/4">
+            <h3 className="font-display mb-1 text-2xl tracking-wider uppercase">
+              Contact
+            </h3>
+            <p>
+              <a href={`tel:${data.site.siteMetadata.phone}`}>
+                {data.site.siteMetadata.phoneFormatted}
+              </a>
+            </p>
+            <p>
+              <a href={`mailto:${data.site.siteMetadata.email}`}>
+                {data.site.siteMetadata.email}
+              </a>
+            </p>
+            <p>
+              <a href="https://m.me/chopandchill">Message Us</a>
+            </p>
           </div>
-          <div className="px-3">
-            <h3 className="font-display text-2xl">Hours</h3>
+          <div className="flex flex-col items-center mb-6 px-3 w-full sm:w-1/4">
+            <h3 className="font-display mb-1 text-2xl tracking-wider uppercase">
+              Hours
+            </h3>
+            <p>
+              <strong>Mon to Sat</strong>
+            </p>
+            <p>
+              Breakfast, <br />
+              Lunch &amp; Dinner
+            </p>
+            <p>
+              <strong>Sunday</strong>
+            </p>
+            <p>Breakfast, Lunch</p>
           </div>
-          <div className="px-3">
-            <h3 className="font-display text-2xl">Navigation</h3>
+          <div className="flex flex-col items-center mb-6 px-3 w-full sm:w-1/4">
+            <h3 className="font-display mb-1 text-2xl tracking-wider uppercase">
+              Navigation
+            </h3>
+            <div className="flex flex-wrap">
+              <ul>
+                <li>
+                  <Link>Home</Link>
+                </li>
+                <li>
+                  <Link>Menu</Link>
+                </li>
+                <li>
+                  <Link>About Us</Link>
+                </li>
+              </ul>
+              <ul className="ml-auto">
+                <li>Functions</li>
+                <li>Contact</li>
+              </ul>
+            </div>
           </div>
+          <img
+            className="absolute h-24 -mb-4 mr-1 right-0 bottom-0"
+            src={Rosemary}
+            alt=""
+          />
         </div>
         <div className="flex flex-wrap items-center justify-center mb-0 p-4 text-center w-full">
           © {new Date().getFullYear()} {data.site.siteMetadata.title}{' '}
