580ac06b6b9f2c45a2824d20b9acb9a75bc5a4b9
Luke Bennett <luke.bennett88@gmail.com>
2019-05-02 10:07:03 +1000

    Create hamburger menu on small screens

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { graphql, Link, StaticQuery } from 'gatsby';
-import { FaFacebookSquare, FaInstagram, FaPhoneSquare } from 'react-icons/fa';
+import { FaFacebookSquare, FaInstagram } from 'react-icons/fa';
 
 import LogoLandscape from '../../images/logo-landscape.svg';
+
+const handleClick = e => {
+  e.preventDefault();
+  const element = document.getElementById('nav');
+  element.classList.toggle('block');
+  element.classList.toggle('hidden');
+};
 
 const Header = () => (
   <StaticQuery
@@ -21,34 +28,53 @@ const Header = () => (
       }
     `}
     render={data => (
-      <header className="bg-brand-blue flex font-sans items-center justify-between leading-none sticky text-white top-0 w-full">
-        <div className="px-6 py-4">
-          <Link
-            to="/"
-            className="flex font-bold items-center no-underline text-lg tracking-tight"
-          >
-            <img
-              className="flex-shrink-0 h-12 py-2"
-              src={LogoLandscape}
-              alt={data.site.siteMetadata.title}
-            />
-          </Link>
+      <header className="bg-brand-blue sm:flex items-center leading-none text-white top-0 relative sticky w-full z-50">
+        <div className="relative">
+          <div className="flex justify-between w-full">
+            <Link
+              to="/"
+              className="flex font-bold items-center no-underline px-6 py-4 text-lg tracking-tight"
+            >
+              <img
+                className="h-12 py-2"
+                src={LogoLandscape}
+                alt={data.site.siteMetadata.title}
+              />
+            </Link>
+
+            <button
+              className="flex sm:hidden items-center justify-center px-6 py-4"
+              onClick={handleClick}
+              type="button"
+            >
+              <svg
+                className="fill-current h-4 w-4"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
         </div>
         <nav
           id="nav"
-          className="flex items-center justify-end px-6 py-4 text-sm sm:text-base tracking-wider uppercase"
+          className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-end pb-2 sm:pb-0 tracking-wider relative uppercase w-full sm:w-auto"
         >
           <NavLink link="/menu" title="Menu" />
           <NavLink link="/about" title="About" />
           <NavLink link="/contact" title="Contact" />
           <a
-            className="hidden sm:block"
+            className="block px-6 sm:px-2 py-2"
             href={`tel:${data.site.siteMetadata.phone}`}
           >
             Call {data.site.siteMetadata.phoneFormatted}
           </a>
-          <FaInstagram className="ml-1" />
-          <FaFacebookSquare className="ml-1" />
+          <div className="flex px-6 py-2">
+            <FaInstagram className="mr-1" />
+            <FaFacebookSquare className="" />
+          </div>
         </nav>
       </header>
     )}
@@ -56,7 +82,7 @@ const Header = () => (
 );
 
 const NavLink = props => (
-  <Link to={props.link} className="px-2">
+  <Link to={props.link} className="block px-6 sm:px-2 py-2">
     {props.title}
   </Link>
 );
