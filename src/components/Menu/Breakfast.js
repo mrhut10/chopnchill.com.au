8f8d52e98a33e4185e2e299ce222639b6978d7ca
Luke Bennett <hello@lukebennett.com.au>
2019-05-10 10:21:48 +1000

    Add Breakfast menu

--- src/components/Menu/Breakfast.js
+++ src/components/Menu/Breakfast.js
@@ -2,14 +2,101 @@ import React from 'react';
 import MenuItem from './MenuItem';
 
 const Breakfast = () => (
-  <MenuItem title="Breakfast" price="?">
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit voluptatem
-      fuga, magnam omnis necessitatibus hic quia delectus culpa quo deleniti,
-      voluptates fugiat excepturi. Libero accusantium necessitatibus expedita
-      nesciunt alias quibusdam.
-    </p>
-  </MenuItem>
+  <>
+    <MenuItem
+      title="BAKED AVOCADO BOATS WITH
+  POACHED EGGS"
+      price="20"
+    >
+      <p>
+        Soft-Poached Eggs in Oven-Baked Avocado Boats. Served with Sourdough
+        Toast Locally-Made Sweet Caramelized Breakfast Chutney.
+      </p>
+    </MenuItem>
+    <MenuItem title="CURRIED PRATA with EGG SOLDIERS" price="XX">
+      <p>
+        A delicious Dipping Dish of Curried Prata, Poached Egg and Locally-Made
+        Sweet Caramelized Breakfast Chutney with Toasted Sourdough and Roti
+        Bread Soldiers.
+      </p>
+    </MenuItem>
+    <MenuItem title="CHOP ‘n CHILL TOWN GREEN" price="17">
+      <p>
+        An easy Breakfast Dish of Smashed Avocado, Roasted Tomatoes, Goat’s
+        Cheese & Pesto on Thick White Toast.
+      </p>
+    </MenuItem>
+    <MenuItem title="CHOP CHOP BOWL" price="XX">
+      <p>
+        Wholesome Dish of Scrambled Eggs, Smashed Avocado, Chilli Beans, Pickled
+        Vegetables, Grated Cheese and Shredded Lettuce. Served with Soft
+        Tortillas and Sriracha Mayonnaise.
+      </p>
+    </MenuItem>
+    <MenuItem title="PORK BREKKY BURRITO" price="22">
+      <p>
+        Smoked Pulled-Pork Burrito with Soft-Poached Eggs, Lettuce, Tomato and
+        Avocado Spread. Served with Lazy Paul’s BBQ Sauce ‘n Mild Chipotle
+        Cream.
+      </p>
+    </MenuItem>
+    <MenuItem title="BACON and EGG ROLL" price="13">
+      <p>
+        Bacon ‘n Fried Egg with Lazy Paul’s BBQ Sauce on a Fresh Hi-top Bun,
+        with a side-serving of Crispy Potato Chunks.
+      </p>
+    </MenuItem>
+    <MenuItem
+      title="EGGS BENEDICT:
+WITH HAM HOCKS, BACON OR SALMON"
+      price="23"
+    >
+      <p>
+        Toasted Local Bagels with your choice of Ham Hocks, Crispy Bacon or
+        Smoked Salmon. Topped with Poached Eggs and Hollandaise Sauce.
+      </p>
+    </MenuItem>
+    <MenuItem title="Stack" price="XX">
+      <p>
+        Thick Sliced of Toast stacked with Rangers Valley marble-five-score
+        Wagyu Beef Kofta Pattie; Smoked Seasoned Sausage; Fried Egg; Chilli
+        Beans 'n Spinach Leaves. Comes with Lazy Paul’s BBQ Sauce.
+      </p>
+    </MenuItem>
+    <MenuItem title="BOARDRIDER’S BRUSCHETTA" price="17">
+      <p>
+        Bacon and Poached Eggs with Bruschetta Tomatoes on Sourdough Toast and
+        Balsamic Glaze.
+      </p>
+    </MenuItem>
+    <MenuItem title="THE NEW YORKER" price="22">
+      <p>
+        A big helping of Smoked Brisket with Crispy Potato Chunks, Poached Eggs,
+        Pesto & Lazy Paul’s BBQ Sauce.
+      </p>
+    </MenuItem>
+    <MenuItem title="TOAST ON THE GO" price="8">
+      <p>
+        Choose from Thick White/Sourdough/ Fruit Loaf/ Banana Bread or
+        Croissant. All with a choice of condiments. Add any extras.
+      </p>
+    </MenuItem>
+    <MenuItem title="CnC’s HUEVOS RANCHEROS" price="XX">
+      <p>
+        Lightly-Fried Rice Paper Basket filled with House-Made Chilli Beans,
+        topped with Shredded Cheese ‘n Fried Eggs.
+      </p>
+    </MenuItem>
+    <MenuItem title="CONTINENTAL WAFFLES" price="17">
+      <p>
+        Toasted Belgian Waffles topped with Wild Berries, Whipped Cream and
+        Canadian Maple Syrup.
+      </p>
+    </MenuItem>
+    <MenuItem title="THE BIG BREKKY PLATTER FOR TWO OR MORE" price="XX">
+      <p>Description to follow</p>
+    </MenuItem>
+  </>
 );
 
 export default Breakfast;
