0b347b5bdf6c3a7a3e3149165c0a3091f7842071
Luke Bennett <hello@lukebennett.com.au>
2019-05-14 14:12:15 +1000

    Add font size to fix spacing issue on sm screens

--- src/components/Menu/Tab.js
+++ src/components/Menu/Tab.js
@@ -27,7 +27,7 @@ class Tab extends Component {
 
     return (
       <li
-        className={`${className} border-brand-blue border-b border-l-2 border-t flex font-display items-center leading-none px-2 py-4 uppercase`}
+        className={`${className} border-brand-blue border-b border-l-2 border-t flex font-display items-center leading-none px-2 py-4 text-base uppercase`}
         onClick={onClick}
       >
         {label}
