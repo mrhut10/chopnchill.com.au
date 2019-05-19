07c3b61493753f610755cdc18abb6505a15062cc
mrhut10 <mrhut10@protonmail.com>
2019-05-20 02:10:25 +1000

    menu page will automatically inject #menu into url now

--- src/templates/menu.js
+++ src/templates/menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { graphql, StaticQuery } from 'gatsby';
 import propTypes from 'prop-types';
 import slugify from 'slugify';
@@ -14,6 +14,7 @@ import Breakfast from '../components/Menu/Breakfast';
 import VeganVegetarian from '../components/Menu/VeganVegetarian';
 import GlutenFree from '../components/Menu/GlutenFree';
 import Drinks from '../components/Menu/Drinks';
+import { navigate } from '@reach/router';
 
 const contentPaths = [
   ['', ToShare],
@@ -67,55 +68,66 @@ const pageQuery = graphql`
   }
 `;
 
-const MenuPage = ({ pageContext }) => (
-  <StaticQuery
-    query={pageQuery}
-    render={data => (
-      <Layout>
-        <SEO
-          title="Menu"
-          keywords={[
-            `Chop 'n Chill`,
-            `Chop and Chill`,
-            `Asian food`,
-            `Burgers`,
-            `Bar`,
-            `Grill`,
-            `restaurant,`,
-          ]}
-        />
-        <div className="bg-brand-blue relative">
-          <Img
-            className="opacity-75"
-            style={{ height: `50vh`, minHeight: `22rem` }}
-            fluid={data.hero.childImageSharp.fluid}
+const MenuPage = ({ pageContext }) => {
+  useEffect(() => {
+    navigate('#menu');
+  }, []);
+  return (
+    <StaticQuery
+      query={pageQuery}
+      render={data => (
+        <Layout>
+          <SEO
+            title="Menu"
+            keywords={[
+              `Chop 'n Chill`,
+              `Chop and Chill`,
+              `Asian food`,
+              `Burgers`,
+              `Bar`,
+              `Grill`,
+              `restaurant,`,
+            ]}
           />
-          <div className="absolute flex inset-0 items-center max-w-3xl mx-auto p-8">
-            <div className="leading-tight max-w-xs text-white text-xl">
-              <h1 className="font-display mb-3 text-6xl">Food</h1>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Eligendi aspernatur optio libero illo nisi rerum. Distinctio
-                expedita est totam illum quod. Consequatur commodi recusandae
-                veniam ratione aspernatur est, beatae tempore.
-              </p>
+          <div className="bg-brand-blue relative">
+            <Img
+              className="opacity-75"
+              style={{ height: `50vh`, minHeight: `22rem` }}
+              fluid={data.hero.childImageSharp.fluid}
+            />
+            <div className="absolute flex inset-0 items-center max-w-3xl mx-auto p-8">
+              <div className="leading-tight max-w-xs text-white text-xl">
+                <h1 className="font-display mb-3 text-6xl">Food</h1>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Eligendi aspernatur optio libero illo nisi rerum. Distinctio
+                  expedita est totam illum quod. Consequatur commodi recusandae
+                  veniam ratione aspernatur est, beatae tempore.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <DumbTabs
-          TabArray={data.allMenuJson.edges.map(tab => {
-            const label = tab.node.title;
-            const key = tab.node.id;
-            const path = `/menu/${slugify(label, { lower: true })}#menu`;
-            const active = pageContext.selectedMenuTab === label;
-            const Content = LabelToContent(label);
-            return DumbTab({ label, key, path, active, Content: <Content /> });
-          })}
-        />
-      </Layout>
-    )}
-  />
-);
+          <DumbTabs
+            TabArray={data.allMenuJson.edges.map(tab => {
+              const label = tab.node.title;
+              const key = tab.node.id;
+              const path = `/menu/${slugify(label, { lower: true })}#menu`;
+              const active = pageContext.selectedMenuTab === label;
+              const Content = LabelToContent(label);
+              return DumbTab({
+                label,
+                key,
+                path,
+                active,
+                Content: <Content />,
+              });
+            })}
+          />
+        </Layout>
+      )}
+    />
+  );
+};
 
 MenuPage.propTypes = {
   pageContext: propTypes.any,
