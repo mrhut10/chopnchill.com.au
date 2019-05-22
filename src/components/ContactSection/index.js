1f72a9634c152157994f01a56479ded5bdb7aefb
Luke Bennett <hello@lukebennett.com.au>
2019-05-22 13:39:58 +1000

    2 column layout

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -11,46 +11,41 @@ const ContactSection = () => (
       query ContactSectionQuery {
         site {
           siteMetadata {
-            phone
-            phoneFormatted
+            email
           }
         }
       }
     `}
     render={data => (
-      <section className="max-w-5xl mx-auto overflow-hidden pb-24 pl-4 w-full">
-        <div className="max-w-4xl mr-auto">
-          <div className="flex items-start">
-            <div className="text-xl w-full">
-              <img
-                className="sm:hidden p-6 mx-auto w-full"
-                src={ContactMobileImage}
-                alt=""
-              />
-              <div className="squiggle bg-left leading-tight max-w-lg mb-4 mr-auto pl-6 sm:pl-12 pr-4">
-                <div className="mb-4">
-                  <h2 className="font-display leading-none my-6 text-5xl text-center uppercase w-full">
-                    Contact Us
-                  </h2>
-                  <p>
-                    We are located on the Town Green overlooking the Hastings
-                    River, call us on 
-                    <a href={`tel:${data.site.siteMetadata.phone}`}>
-                      {data.site.siteMetadata.phoneFormatted}
-                    </a>{' '}
-                    or complete the enquiry form below. Call or email with your
-                    questions and queries. We look forward to hearing from you!
-                  </p>
-                </div>
-                <h3 className="text-3xl uppercase">Enquiry</h3>
-                <ContactForm />
-              </div>
-            </div>
+      <section className="max-w-5xl mx-auto overflow-hidden pb-24 pr-4 w-full">
+        <img
+          className="md:hidden max-w-lg mx-auto p-6 w-full"
+          src={ContactMobileImage}
+          alt=""
+        />
+        <div className="squiggle bg-right flex flex-wrap px-4">
+          <div className="leading-tight max-w-xl mx-auto px-4 text-2xl w-full md:w-1/2">
+            <h2 className="font-display leading-none mb-6 text-5xl text-center uppercase w-full">
+              Contact Us
+            </h2>
+            <p className="mb-4">
+              We are located on the Town Green overlooking the Hastings River,
+              call us on{' '}
+              <a href={`tel:${data.site.siteMetadata.phone}`}>
+                {data.site.siteMetadata.phoneFormatted}
+              </a>{' '}
+              or complete the enquiry form below. Call or email with your
+              questions and queries. We look forward to hearing from you!
+            </p>
             <img
-              className="hidden sm:block md:max-w-xs ml-4 mr-6 w-64 md:w-full"
+              className="hidden md:block mx-auto p-6 w-64"
               src={ContactDesktopImage}
               alt=""
             />
+          </div>
+          <div className="max-w-md mx-auto px-4 w-full md:w-1/2">
+            <h2 className="text-3xl uppercase">Enquiry</h2>
+            <ContactForm />
           </div>
         </div>
       </section>
